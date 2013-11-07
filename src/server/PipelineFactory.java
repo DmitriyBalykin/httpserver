c6c8ec0ff1a8907161b919c66504e7584d628fdd
Dmitriy Balykin <b_dmitriy82@mail.ru>
2013-11-07 16:06:05 +0200

    Modify server pipeline

--- src/server/PipelineFactory.java
+++ src/server/PipelineFactory.java
@@ -1,15 +1,11 @@
 package server;
 
-import server.diff.TimeServerHandler;
-import server.handler.Aggregator;
-import server.handler.ChunkedWriteHandlerProxy;
-import server.handler.DecoderProxy;
-import server.handler.HttpRequestHandler;
-import server.handler.ResponseEncoderProxy;
-import server.handler.StatCollectorHandler;
-import server.utils.StatCollector;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpServerCodec;
+import server.handler.HttpRequestHandler;
+import server.handler.StatCollectorHandler;
+import server.utils.StatCollector;
 
 public class PipelineFactory {
 	
@@ -17,16 +13,18 @@ public class PipelineFactory {
 	public ChannelPipeline getPipeline(Channel channel, StatCollector statCollector){
 		ChannelPipeline pipeline = channel.pipeline();
 		
-/*		pipeline.addLast(new StatCollectorHandler(statCollector));
-		pipeline.addLast(new DecoderProxy());
+		pipeline.addLast(new StatCollectorHandler(statCollector));
+/*		pipeline.addLast(new DecoderProxy());
 		pipeline.addLast(new Aggregator());
 		pipeline.addLast(new ResponseEncoderProxy());
-		pipeline.addLast(new ChunkedWriteHandlerProxy());
+		pipeline.addLast(new ChunkedWriteHandlerProxy());*/
+		pipeline.addLast(new HttpServerCodec());
 		
-		pipeline.addLast(new HttpRequestHandler(statCollector));*/
+		pipeline.addLast(new HttpRequestHandler(statCollector));
 		
-		pipeline.addLast(new TimeServerHandler());
+//		pipeline.addLast(new TimeServerHandler());
 		
 		return pipeline;
 	}
 }
+
